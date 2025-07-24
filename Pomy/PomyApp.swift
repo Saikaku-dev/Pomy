f92b186adfcb66e8ef42fe7353f4f239dd65aa3b
Saikaku <grnmnsta@gmail.com>
2025-07-24 15:44:11 +0900

    first commit

---
+++ Pomy/PomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomyApp.swift
+//  Pomy
+//
+//  Created by cmStudent on 2025/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
